17e1dda85114a7323709b9db9381f820665b2656
levin <levincao@gmail.com>
2020-04-20 19:20:21 +0800

    edit db column

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, Column, String, Integer, Float, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, Numeric, ForeignKey
 import pymysql
 
 # Connect to DB
@@ -14,13 +14,13 @@ session = Session()
 class AsbFundUnitPrice(Base):
     __tablename__ = 'asb_funds_unit_price'
 
-    id = Column(Integer, primary_key=True)
-    day = Column(Integer)
-    month = Column(Integer)
-    year = Column(Integer)
-    week = Column(Integer)
-    fund_id = Column(Integer, ForeignKey('asb_funds_name.id'))
-    fund_price = Column(Float)
+    id = Column(Integer(), primary_key=True)
+    day = Column(Integer())
+    month = Column(Integer())
+    year = Column(Integer())
+    week = Column(Integer())
+    fund_id = Column(Integer(), ForeignKey('asb_funds_name.id'))
+    fund_price = Column(Numeric(8, 4))
 
     def __repr__(self):
         return "%s Unit price is %s at %s-%s-%s" % (self.fund_id, self.fund_price, self.year, self.month, self.day)
@@ -29,8 +29,8 @@ class AsbFundUnitPrice(Base):
 class AsbFundsName(Base):
     __tablename__ = 'asb_funds_name'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(50))
 
     def __repr__(self):
         return "Fund %s is %s" % (self.name, self.id)
